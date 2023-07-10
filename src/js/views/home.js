74ade8b9615cd346f600ad6ad5f93b6c5e4b9fba
PIERINA BRITO <pierina.brito@tbiil.edu>
2023-07-10 11:26:38 -0500

    first commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,17 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
+import { SWCharacters } from "../component/SWCharacters";
+import { SWPlanets } from "../component/SWPlanets";
+import { SWVehicles } from "../component/SWVehicles";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+  return (
+    <div className="text-center mt-5">
+      <SWCharacters />
+      <SWPlanets />
+      <SWVehicles />
+    </div>
+  );
+};
